b978ed3f25bada0b0ccf9f90b4d3a64150870b19
Nugrawan <126413672+Nugrawan@users.noreply.github.com>
2023-08-09 23:20:56 +0700

    Initialize repository

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        
+      },
+    },
+    fontFamily: {
+      'font-DM': ['DM Mono', 'monospace'],
+      'font-ibm-mono': ['IBM Plex Mono', 'monospace'],
+      'font-ibm-sans': ['IBM Plex Sans', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
